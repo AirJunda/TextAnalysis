7be1c44be97cf72dc69d2fec3f8adada70966061
AirJunda <junda.li1995@gmail.com>
2019-10-24 20:07:31 -0400

    1st handin for ass3

--- main_a3.py
+++ main_a3.py
@@ -9,28 +9,27 @@ Text (not nltk.text.Text).
 
 """
 import os
-#import math
 import re
-
 import nltk
-#from nltk.corpus import brown
+
+
 from nltk.corpus import wordnet as wn
 from nltk.corpus import PlaintextCorpusReader
 from nltk.probability import FreqDist
-#from nltk.text import Text
 
-states = ['S0', 'S1', 'S2']
-final_states = ['S2']
-transitions = [ ('S0', 'a', 'S1'), ('S1', 'b', 'S1'), ('S1', 'c', 'S2') ]
+"""Uncomment below if errors raised"""
+#nltk.download('punkt')
+#nltk.download('wordnet')
 
-#def FSA('test', states, final_states, transitions):
+
     
 class FSA():
-    def __init__(self, states, final_states, transitions):
+    def __init__(self, name ,states, final_states, transitions):
         self.states = states
         self.final_states = final_states
         self.transitions = transitions
         self.table = self.buildTable()
+        self.name = name
  
         
     def buildTable(self):
@@ -45,13 +44,14 @@ class FSA():
     def pp(self):
         # prety print
         for state in self.table:
-            if state in final_states:
+            if state in self.final_states:
                 print("<State " + state + " f>")
             else:
                 print("<State " + state + ">")
             for key in self.table[state]:
-                print(key + " --> "+  self.table[state][key])
+                print("  "+ key + " --> "+  self.table[state][key])
  
+    
     def accept(self,string):
         idx = 0
         curstate = self.states[0]
@@ -64,12 +64,15 @@ class FSA():
                 idx += 1
             else:
                 return False
-        if not curstate in self.final_states:  # or in states??
+        if not curstate in self.final_states:
             return False
         else:
             return True
-        
-    def accept2(self,start,tokens):
+    
+    
+    def fsaMatch(self,start,tokens):
+        # search a fsa pattern in a list of tokens from start index
+        # if no match, return False
         idx = start
         curstate = self.states[0]
         table = self.table
@@ -86,18 +89,18 @@ class FSA():
         if not curstate in self.final_states:
             return False
         else:
-            return start, idx  # 这里的idx刚好是(match的idx +1)  这里返回的是（start,idx）
+            return start, idx  # idx is the last matching position + 1
         
         
         
         
-fsa = FSA(states, final_states, transitions)       
+      
     
 
 class Text(object):
     def __init__(self, path):
         if os.path.isfile(path):
-            f = open(path)#'rU')
+            f = open(path,'rU')
             raw = f.read()
             tokens = nltk.word_tokenize(raw)            
         elif os.path.isdir(path):
@@ -105,7 +108,7 @@ class Text(object):
             tokens = []
             for file in filelist:
                 filepath = path +'/'+ file
-                f = open(filepath)#,'rU')
+                f = open(filepath,'rU')
                 raw = f.read()
                 ftok = nltk.word_tokenize(raw)
                 tokens = tokens + ftok
@@ -113,27 +116,17 @@ class Text(object):
         self.text_to_str = raw
         self.text = nltk.Text(tokens)
             
-#        if os.path.isfile(path):
-#            with open(path) as fh:
-#                raw = fh.read()
-#                self.text = nltk.Text(nltk.word_tokenize(raw))
-#                self.text_to_str = fh.read()
-#        elif os.path.isdir(path):
-#            corpus = PlaintextCorpusReader(path, '.*.mrg')
-#            raw = corpus.raw()
-#            self.text = nltk.Text(nltk.word_tokenize(raw))
-#            self.text_to_str = corpus.raw()
+
+        
     def apply_fsa(self,fsa):
         all_tokens = self.text.tokens
         result = []
         i = 0
-        while i < len(all_tokens):
-            
-            if fsa.accept2(i,all_tokens) != False:
-                start,end = fsa.accept2(i,all_tokens)
-    
-            target = all_tokens[start:end]
-            result.append((start, " ".join(target)))
+        while i < len(all_tokens):            
+            if fsa.fsaMatch(i,all_tokens) != False:
+                start,end = fsa.fsaMatch(i,all_tokens)
+                target = all_tokens[start:end]
+                result.append((start, " ".join(target)))
             i += 1
         
         return result
@@ -141,7 +134,7 @@ class Text(object):
 
     def __len__(self):
         return len(self.text_to_str)
-        #return len(self.text)
+        #Alternative, we can return len(self.text) which make senses too.
          
     def token_count(self):
         return len(self.text)
@@ -157,7 +150,7 @@ class Text(object):
                 count = count + 1
         return count
     
-    def most_frequent_content_words(self): # 有点问题回头修改下
+    def most_frequent_content_words(self):  # This function has been edited from ass2 version
         text = self.text           
         result = []
         feqlist = FreqDist(text)
@@ -169,14 +162,14 @@ class Text(object):
         
         while count < 25 and i < len(tokens):
             token = tokens[i]
-            if token.lower() not in stopwords and token.isalnum():
+            if token.lower() not in stopwords and token[0].isalpha():   # here we use our teacher's definition of content word
                 result.append( (token,feqlist[token]) )
                 count = count + 1
             i = i + 1
         return result
             
     
-    def most_frequent_bigrams(self):  # 没什么问题。
+    def most_frequent_bigrams(self):
         text = self.text
         allBG = list(nltk.bigrams(text))
         feqlist = FreqDist(allBG)
@@ -215,8 +208,7 @@ class Text(object):
         #  returns a sorted list of all sirs
         result = []
         rawtext = self.text_to_str
-        pattern = re.compile(r"Sir \S+[\w]") #  This may also works "\bSir \S+\b"
-        #\bSir \S+\b#
+        pattern = re.compile(r"\bSir \S+\b") # Alternative: "Sir \S+[\w]"
         result = pattern.findall(rawtext)
         result = set(result)
         result = list(result)
@@ -240,18 +232,16 @@ class Text(object):
         result = pattern.findall(rawtext)
         result = set(result)
         result = list(result)       
-        result = [ w[0:-1] for w in result]
-        
+        result = [ w[0:-1] for w in result]        
         result.sort()
         return result
     
     def find_repeated_words(self): 
         #returns a sorted list of words repeated three times
         rawtext = self.text_to_str
-        pattern = re.compile(r"(\b(\w+)\b \2\b \2\b)")   # "\b(\w+)\b \1\b \1\b"  OR  "\b(\w+)\s+\1\b\s+\1\b"  MULTIIME: (\b\w+\b)(\s+\1)+
+        pattern = re.compile(r"(\b(\w+)\b \2\b \2\b)") 
         result = pattern.findall(rawtext)
-        fullmatc = [ groups[0] for groups in result]
-        
+        fullmatc = [ groups[0] for groups in result]        
         fullmatc = set(fullmatc)
         fullmatc = list(fullmatc)
         fullmatc.sort()
@@ -292,16 +282,10 @@ class Vocabulary(object):
         syns = wn.synsets(word)
         return syns[0].definition()
     
-    def quick(self, word): # same
+    def quick(self, word):
         self.text.concordance(word)        
         
-#     
-grail = Text('data/grail.txt')
-#sirs = grail.find_sirs()
-#bracks = grail.find_brackets()
-#repeats = grail.find_repeated_words()
-#roles = grail.find_roles()
-#        
-#
-#emma = Text('data/emma.txt')
+        
+
+        
 
